ef5c1cfa561287559d6381bf7cc5eb83d80e69a4
Marc ETAVARD <rakushain@gmail.com>
2024-05-13 09:32:21 +0200

    CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     "postgresql://postgres:postgres@localhost:5432/logistisen_db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+CORS(app)
 db = SQLAlchemy(app)
