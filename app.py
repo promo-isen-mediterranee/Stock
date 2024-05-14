ad31c113d05a7043967702eb7915721e1f868d06
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2024-05-14 15:39:05 +0200

    fix: adding missing files

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 CORS(app)
 db = SQLAlchemy(app)
+
+if __name__ == "__main__":
+    app.run()
