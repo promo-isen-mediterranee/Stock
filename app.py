58e92f48c174e81ee1cef3b41c8d37e715f7e048
Marc ETAVARD <rakushain@gmail.com>
2024-05-14 15:38:05 +0200

    Fix2

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
 import os
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 load_dotenv('.flaskenv')
 
